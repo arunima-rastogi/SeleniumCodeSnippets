f7783f0e7a864c6f5f35e39f30f52255c7200f0c
Arunima <e.arastogi@gmail.com>
2019-08-03 18:37:46 +0530

    create directory structure

---
+++ src/test/java/CodeSnippets/create_directory.java
@@ -0,0 +1,26 @@
+package CodeSnippets;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class create_directory {
+
+	public static void main(String[] args) {
+		String current_DateTime = new SimpleDateFormat("MM-dd-yyyy_HH-mm-ss").format(new Date().getTime());
+		String dir_name = ".\\dir_"+current_DateTime;
+		File f = new File(dir_name);
+		if(!f.exists())
+		{
+			f.mkdir();
+			System.out.println("Directory created");
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+			System.out.println("After Format : " + sdf.format(f.lastModified()));
+			System.out.println("File Name: " +f.getName());
+		}
+		
+		
+
+	}
+
+}
