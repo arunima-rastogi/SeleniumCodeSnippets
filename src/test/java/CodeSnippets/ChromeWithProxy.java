71a4c89a839b131795525e90d5d47b785af47c99
Rastogi <srasz@allstate.com>
2019-08-07 08:45:44 -0400

    updating for Chrome with proxy

---
+++ src/test/java/CodeSnippets/ChromeWithProxy.java
@@ -0,0 +1,27 @@
+package CodeSnippets;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class ChromeWithProxy {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\srasz\\Desktop\\Selenium4mRemote\\chromedriver.exe");
+		WebDriver driver;
+		String proxy = "127.0.0.1:5000";
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-infobars");  //disables chrome is controlled by automated test software
+		options.addArguments("--proxy-server=http://" + proxy);
+		driver = new ChromeDriver(options);
+		
+		driver.get("http://www.google.co.in");
+		
+		driver.quit();
+	}
+
+}
