1cb36c78e2aad07c8ecf9c576220abd316b9330f
Arunima <e.arastogi@gmail.com>
2019-08-27 12:12:23 +0530

    updating @Listeners

--- src/test/java/testNG/ISuiteListener/Examples/ISTL.java
+++ src/test/java/testNG/ISuiteListener/Examples/ISTL.java
@@ -1,7 +1,9 @@
 package testNG.ISuiteListener.Examples;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(CustomListener3.class)
 public class ISTL {
 
 	@Test
