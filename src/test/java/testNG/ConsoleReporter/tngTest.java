0476a4324df184c0cfc1e766a00b58d710ab2148
Rastogi <srasz@allstate.com>
2019-09-04 05:54:06 -0400

    updating console writer

---
+++ src/test/java/testNG/ConsoleReporter/tngTest.java
@@ -0,0 +1,17 @@
+package testNG.ConsoleReporter;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners({ ConsoleReporter.class})
+public class tngTest {
+
+	@Test
+	public void f1() {
+		System.out.println("Hello");
+		assertTrue(true);
+	}
+
+}
