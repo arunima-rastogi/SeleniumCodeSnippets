89ef066e3f277e876f42c4a33c893c6bb20c6453
Rastogi <srasz@allstate.com>
2019-09-06 06:07:52 -0400

    updating

--- src/main/java/UtilityFunctions/DateUtils.java
+++ src/main/java/UtilityFunctions/DateUtils.java
@@ -2,6 +2,7 @@ package UtilityFunctions;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
 import org.joda.time.Period;
 
 import java.text.ParseException;
@@ -16,10 +17,14 @@ import java.util.TimeZone;
  */
 
 public final class DateUtils {
-	
+
 	private static final int END_INDEX = 3;
 	private static final int MULTIPLICATION_FACTOR_TO_CONVERT_MINUTE_TO_MILLISECONDS = 1000;
 	private static final int SECONDS_IN_MINUTE = 60;
+	private static final String[] strDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday",
+			"Friday", "Saturday" };
+	private static final String[] strMonths = new String[] { "January", "February", "March", "April", "May", "June", "July", "August",
+			"September", "October", "November", "December" };
 
 	private DateUtils() {
 	}
@@ -97,7 +102,7 @@ public final class DateUtils {
 	}
 
 	/**
-	 * Creates time zone with DST setting equal to New York 
+	 * Creates time zone with DST setting equal to New York
 	 *
 	 * @param rawOffset - offset for time zone you want to create
 	 * @return - modified TimeZone
@@ -118,7 +123,8 @@ public final class DateUtils {
 		DateTime currentTime = new DateTime(dateTimeZone);
 		DateTime dateWithTimeZone = date.withZoneRetainFields(dateTimeZone);
 		if (dateWithTimeZone.getMillis() > currentTime.getMillis()) {
-			CustomWaits.waitForSomeTime((int) (dateWithTimeZone.getMillis() - currentTime.getMillis()), "Wait for date");
+			CustomWaits.waitForSomeTime((int) (dateWithTimeZone.getMillis() - currentTime.getMillis()),
+					"Wait for date");
 		}
 	}
 
@@ -141,5 +147,29 @@ public final class DateUtils {
 	 */
 	public static int getTimeZoneDifferenceInHours(Date firstDate, DateTime secondDateTime) {
 		return (new Period(firstDate.getTime(), secondDateTime.toDate().getTime())).getHours();
+	}
+
+	public static String getDayOfWeek(String yyyymmdd) {
+		// String should be in YYYY-MM-DD format
+		LocalDate dt = LocalDate.parse(yyyymmdd);
+		return strDays[dt.getDayOfWeek()];
+	}
+
+	public static String getMonthName(String yyyymmdd) {
+		LocalDate dt = LocalDate.parse(yyyymmdd);
+		return strMonths[dt.getMonthOfYear()-1];
+	}
+	
+	public static int getDayOfYear(String yyyymmdd) {
+		LocalDate dt = LocalDate.parse(yyyymmdd);
+		return dt.getDayOfYear();
+	}
+
+	
+	public static void main(String[] args) {
+		String dt = "2019-09-06";
+		System.out.println(getDayOfWeek(dt));
+		System.out.println(getMonthName(dt));
+		System.out.println(getDayOfYear(dt));
 	}
 }
